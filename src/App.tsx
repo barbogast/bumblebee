dbab0e1fbbcab0c88bb6251b25ff66c7bab39330
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2021-11-25 17:58:45 +0100

    Properly display results in UI

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,22 @@ import './App.css';
 import { open } from '@tauri-apps/api/dialog';
 import { invoke } from '@tauri-apps/api/tauri';
 
+type Result = [
+  {
+    missing_in_dir_a: string[];
+    missing_in_dir_b: string[];
+  },
+  { differing_content: string[]; file_and_directory: string[] }
+];
+
 function App() {
   const [pathA, setPathA] = useState<string>();
   const [pathB, setPathB] = useState<string>();
+  const [result, setResult] = useState<Result | void>();
   return (
     <div className='App'>
       <header className='App-header'>
-        {pathA}
+        <input value={pathB} onChange={(e) => setPathA(e.target.value)} />
         <button
           onClick={() =>
             open({ directory: true })
@@ -19,7 +28,7 @@ function App() {
         >
           Set dir A
         </button>
-        {pathB}
+        <input value={pathB} onChange={(e) => setPathB(e.target.value)} />
         <button
           onClick={() =>
             open({ directory: true })
@@ -34,12 +43,20 @@ function App() {
             console.log('invoke');
 
             invoke('compare', { pathA, pathB })
-              .then((message) => console.log(message))
+              .then((message) => setResult(message as Result))
               .catch((e) => console.error(e));
           }}
         >
           Compare
         </button>
+        missing_in_dir_a
+        {result && <textarea value={result[0].missing_in_dir_a.join('\n')} />}
+        missing_in_dir_b
+        {result && <textarea value={result[0].missing_in_dir_b.join('\n')} />}
+        differing_content
+        {result && <textarea value={result[1].differing_content.join('\n')} />}
+        file_and_directory
+        {result && <textarea value={result[1].file_and_directory.join('\n')} />}
       </header>
     </div>
   );
