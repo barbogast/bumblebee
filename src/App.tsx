81c95179329a1577afc38b31475901a170393c15
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2021-11-25 19:42:26 +0100

    Replace textarea with table

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, Table, Alert } from 'antd';
 import './App.css';
 import { open } from '@tauri-apps/api/dialog';
 import { invoke } from '@tauri-apps/api/tauri';
+import { ColumnsType } from 'antd/es/table';
 
 type Result = [
   {
@@ -12,13 +13,101 @@ type Result = [
   { differing_content: string[]; file_and_directory: string[] }
 ];
 
+type Reason = 'missingInA' | 'missingInB' | 'differingContent' | 'fileAndDirectory';
+
+type TableData = {
+  key: string;
+  path: string;
+  reason: Reason;
+};
+
 const { Header, Content, Footer, Sider } = Layout;
+
+const renderTableCell = (text: string, record: TableData) => {
+  const classMap: { [key in Reason]: 'warning' | 'error' } = {
+    missingInA: 'error',
+    missingInB: 'error',
+    differingContent: 'warning',
+    fileAndDirectory: 'warning',
+  };
+  return text ? (
+    <Alert type={classMap[record.reason]} message={text}>
+      {record.reason}
+    </Alert>
+  ) : null;
+};
 
 function App() {
   const [collapsed, setCollapsed] = useState(false);
   const [pathA, setPathA] = useState<string>();
   const [pathB, setPathB] = useState<string>();
   const [result, setResult] = useState<Result | void>();
+
+  const columns: ColumnsType<TableData> = result
+    ? [
+        {
+          title: 'Path',
+          dataIndex: 'path',
+          key: 'path',
+        },
+        {
+          title: 'A',
+          dataIndex: 'dirA',
+          key: 'dirA',
+          render: renderTableCell,
+        },
+        {
+          title: 'B',
+          dataIndex: 'dirB',
+          key: 'dirB',
+          render: renderTableCell,
+        },
+      ]
+    : [];
+
+  const missingInDirA: TableData[] = result
+    ? result[0].missing_in_dir_a.map((path) => ({
+        key: path,
+        path,
+        reason: 'missingInA',
+        dirA: 'File missing',
+      }))
+    : [];
+
+  const missingInDirB: TableData[] = result
+    ? result[0].missing_in_dir_b.map((path) => ({
+        key: path,
+        path,
+        reason: 'missingInB',
+        dirB: 'File missing',
+      }))
+    : [];
+
+  const differintContent: TableData[] = result
+    ? result[1].differing_content.map((path) => ({
+        key: path,
+        path,
+        reason: 'differingContent',
+        dirA: 'Differing content',
+        dirB: 'Differing content',
+      }))
+    : [];
+
+  const fileAndDirectory: TableData[] = result
+    ? result[1].file_and_directory.map((path) => ({
+        key: path,
+        path,
+        reason: 'fileAndDirectory',
+        dirA: ' File or directory',
+        dirB: 'File or direcotry',
+      }))
+    : [];
+
+  const tableData = missingInDirA
+    .concat(missingInDirB)
+    .concat(differintContent)
+    .concat(fileAndDirectory);
+
   return (
     <Layout style={{ minHeight: '100vh' }}>
       <Sider collapsible collapsed={collapsed} onCollapse={() => setCollapsed(!collapsed)}>
@@ -67,22 +156,7 @@ function App() {
             >
               Compare
             </button>
-            <div>
-              missing_in_dir_a
-              {result && <textarea value={result[0].missing_in_dir_a.join('\n')} disabled />}
-            </div>
-            <div>
-              missing_in_dir_b
-              {result && <textarea value={result[0].missing_in_dir_b.join('\n')} disabled />}
-            </div>
-            <div>
-              differing_content
-              {result && <textarea value={result[1].differing_content.join('\n')} disabled />}
-            </div>
-            <div>
-              file_and_directory
-              {result && <textarea value={result[1].file_and_directory.join('\n')} disabled />}
-            </div>
+            {result && <Table dataSource={tableData} columns={columns} />}
           </div>
         </Content>
         <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
