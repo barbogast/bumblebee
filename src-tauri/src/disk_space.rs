6cf127e57bc1843166883ac58d6de7e7b325700c
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2021-12-08 12:49:28 +0100

    Replace anonymous structs with named ones

--- src-tauri/src/disk_space.rs
+++ src-tauri/src/disk_space.rs
@@ -6,24 +6,33 @@ use std::{fs, io};
 use tauri::Manager;
 
 #[derive(Debug, Eq, PartialEq, Clone, serde::Serialize, serde::Deserialize)]
+pub struct DirEntry {
+    path: String,
+    size: u64,
+    number_of_files: u64,
+    content: Vec<Entry>,
+}
+
+#[derive(Debug, Eq, PartialEq, Clone, serde::Serialize, serde::Deserialize)]
+pub struct ErrorEntry {
+    path: Option<String>,
+    size: Option<u64>,
+    content: Option<Vec<Entry>>,
+    reason: String,
+}
+
+#[derive(Debug, Eq, PartialEq, Clone, serde::Serialize, serde::Deserialize)]
+pub struct FileEntry {
+    path: String,
+    size: u64,
+}
+
+#[derive(Debug, Eq, PartialEq, Clone, serde::Serialize, serde::Deserialize)]
 #[serde(tag = "type")]
-enum Entry {
-    File {
-        path: String,
-        size: u64,
-    },
-    Dir {
-        path: String,
-        size: u64,
-        number_of_files: u64,
-        content: Vec<Entry>,
-    },
-    Error {
-        path: Option<String>,
-        size: Option<u64>,
-        content: Option<Vec<Entry>>,
-        reason: String,
-    },
+pub enum Entry {
+    File(FileEntry),
+    Dir(DirEntry),
+    Error(ErrorEntry),
 }
 
 struct Context<'a, 'b> {
@@ -34,19 +43,17 @@ struct Context<'a, 'b> {
 impl Entry {
     fn size(&self) -> u64 {
         match self {
-            Entry::File { size, .. } => *size,
-            Entry::Dir { size, .. } => *size,
-            Entry::Error { .. } => 0,
+            Entry::File(f) => f.size,
+            Entry::Dir(d) => d.size,
+            Entry::Error(_) => 0,
         }
     }
 
     fn number_of_files(&self) -> u64 {
         match self {
-            Entry::File { .. } => 1,
-            Entry::Error { .. } => 1,
-            Entry::Dir {
-                number_of_files, ..
-            } => *number_of_files,
+            Entry::File(_) => 1,
+            Entry::Error(_) => 1,
+            Entry::Dir(d) => d.number_of_files,
         }
     }
 }
@@ -55,25 +62,29 @@ fn analyse_entry(
     context: &mut Context,
     entry: Result<fs::DirEntry, io::Error>,
 ) -> Result<Entry, Entry> {
-    let entry = entry.map_err(|err| Entry::Error {
-        path: None,
-        size: None,
-        content: None,
-        reason: err.to_string(),
+    let entry = entry.map_err(|err| {
+        Entry::Error(ErrorEntry {
+            path: None,
+            size: None,
+            content: None,
+            reason: err.to_string(),
+        })
     })?;
 
-    let metadata = entry.metadata().map_err(|err| Entry::Error {
-        path: Some(entry.path().to_string_lossy().to_string()),
-        size: None,
-        content: None,
-        reason: err.to_string(),
+    let metadata = entry.metadata().map_err(|err| {
+        Entry::Error(ErrorEntry {
+            path: Some(entry.path().to_string_lossy().to_string()),
+            size: None,
+            content: None,
+            reason: err.to_string(),
+        })
     })?;
 
     if metadata.is_file() {
-        Ok(Entry::File {
+        Ok(Entry::File(FileEntry {
             path: entry.path().to_string_lossy().to_string(),
             size: metadata.len(),
-        })
+        }))
 
         // TODO: is_symlink is unstable
         // } else if metaadata.is_symlink() {
@@ -86,23 +97,23 @@ fn analyse_entry(
 fn analyze_directory_recursive<P: AsRef<Path>>(context: &mut Context, directory_path: P) -> Entry {
     let path_str = directory_path.as_ref().to_string_lossy().to_string();
     if context.should_abort.0.load(atomic::Ordering::Relaxed) {
-        return Entry::Error {
+        return Entry::Error(ErrorEntry {
             path: Some(path_str),
             size: None,
             content: None,
             reason: "Aborted".to_string(),
-        };
+        });
     }
     context.report_progress.maybe_run(path_str.clone());
 
     let read_dir = fs::read_dir(directory_path);
     if let Err(err) = read_dir {
-        return Entry::Error {
+        return Entry::Error(ErrorEntry {
             path: Some(path_str),
             size: None,
             content: None,
             reason: err.to_string(),
-        };
+        });
     }
 
     let read_dir = read_dir.unwrap();
@@ -117,12 +128,12 @@ fn analyze_directory_recursive<P: AsRef<Path>>(context: &mut Context, directory_
     let size: u64 = entries.iter().map(|entry| entry.size()).sum();
     let number_of_files = entries.iter().map(|entry| entry.number_of_files()).sum();
 
-    Entry::Dir {
+    Entry::Dir(DirEntry {
         path: path_str,
         content: entries,
         size,
         number_of_files,
-    }
+    })
 }
 
 #[derive(Debug, Eq, PartialEq, Clone, serde::Serialize, serde::Deserialize)]
@@ -153,12 +164,12 @@ pub fn analyze_disk_usage(
 
     if should_abort.0.load(atomic::Ordering::Relaxed) {
         // No sense to send the data collected so far, return an empty result
-        result = Entry::Error {
+        result = Entry::Error(ErrorEntry {
             path: Some(path),
             size: None,
             content: None,
             reason: "Aborted".to_string(),
-        };
+        });
     }
     AnalyseResult {
         result,
